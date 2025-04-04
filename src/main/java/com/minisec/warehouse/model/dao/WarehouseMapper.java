9550b02c542356cbb6af5aa0cfac5824cab6bc8d
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-04-04 17:29:05 +0900

    [Modify] 불필요 코드 제거
    author : 이서연
    - 창고상품조회 업데이트 삭제

--- src/main/java/com/minisec/warehouse/model/dao/WarehouseMapper.java
+++ src/main/java/com/minisec/warehouse/model/dao/WarehouseMapper.java
@@ -8,8 +8,5 @@ import java.util.List;
 public interface WarehouseMapper {
     // 전체 상품 조회
     List<WarehouseDto> getProductList();
-
-    // 입고 수량 db 업데이트
-    void updateStorageQuantity(@Param("storageId") int storageId, @Param("newQuantity") int newQuantity);
 }
 
