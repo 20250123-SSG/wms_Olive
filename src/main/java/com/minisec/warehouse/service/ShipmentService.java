25a057d9464a526d2df5ed4114a43a360ef4be6d
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-04-04 17:26:26 +0900

    [Add] Shipment 발주신청내역확인 추가
    author : 이서연
    - 태호님께서 주신 코드에
    1. Shipment 이전 화면으로 돌아가기
    2. Shipment 수주/거절했으면 db 업데이트
    3. Shipment 상세조회 ShipmentDetailDto 사용
    추가 기능 구현함

---
+++ src/main/java/com/minisec/warehouse/service/ShipmentService.java
@@ -0,0 +1,36 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.minisec.warehouse.service;
+
+import com.minisec.common.Template;
+import com.minisec.warehouse.model.dao.ShipmentDao;
+import com.minisec.warehouse.model.dto.ShipmentDto;
+import java.util.List;
+import org.apache.ibatis.session.SqlSession;
+
+public class ShipmentService {
+    private ShipmentDao shipmentDao;
+
+    public ShipmentService() {
+    }
+
+    public void updateOrderStatus(int orderId, char status) {
+        try (SqlSession sqlSession = Template.getSqlSession()) {
+            shipmentDao = sqlSession.getMapper(ShipmentDao.class);
+            shipmentDao.updateOrderStatus(orderId, status);
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("주문 상태 업데이트 중 오류 발생");
+        }
+    }
+
+    public List<ShipmentDto> getOrderList(int manageId, int choice) {
+        SqlSession sqlSession = Template.getSqlSession();
+        this.shipmentDao = (ShipmentDao)sqlSession.getMapper(ShipmentDao.class);
+        return this.shipmentDao.selectOrderList(manageId, choice);
+    }
+}
