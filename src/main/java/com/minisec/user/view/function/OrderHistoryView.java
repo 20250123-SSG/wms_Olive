d7f936f2b15bea363ad273a84ff9ea118aa63daf
sotogito <sotogitoarchive@gmail.com>
2025-04-07 15:51:01 +0900

    [Modify] 주문 내역 출력 오름차순 기본 출력
    author : 김수연

--- src/main/java/com/minisec/user/view/function/OrderHistoryView.java
+++ src/main/java/com/minisec/user/view/function/OrderHistoryView.java
@@ -3,8 +3,11 @@ package com.minisec.user.view.function;
 import com.minisec.common.login.Login;
 import com.minisec.user.controller.OrderController;
 import com.minisec.user.model.dto.order.OrderDto;
+import com.minisec.user.view.details.InputFunctionNumberView;
+import com.minisec.user.view.printer.ExceptionPrinter;
 import com.minisec.user.view.printer.order.OrderSimplePrinter;
 import com.mysql.cj.x.protobuf.MysqlxCrud;
+import com.sun.source.tree.TryTree;
 
 import java.util.List;
 import java.util.Scanner;
@@ -16,18 +19,23 @@ public class OrderHistoryView {
     private final OrderController orderController = new OrderController();
 
     public void run(Login user) {
-        System.out.println("""
-                1. 누적 구매 내역 조회
-                2. 취소 구매 내역 조회
-                0. 뒤로가기
-                >> 입력:""");
 
-        switch (Integer.parseInt(sc.nextLine())) {
-            case 0:                                  return;
-            case 1: selectOrderList(user);           break;
-            case 2: selectCanceledStatusOrder(user); break;
-            default: return;
-        }
+        while (true) {
+            System.out.println("""
+                    1. 누적 구매 내역 조회
+                    2. 취소 구매 내역 조회
+                    0. 뒤로가기
+                    """);
+
+           switch (InputFunctionNumberView.input()) {
+               case 0:                                  return;
+               case 1: selectOrderList(user);           break;
+               case 2: selectCanceledStatusOrder(user); break;
+               default:
+                   System.out.println("존재하지 않는 기능입니다.");
+                   continue;
+           }
+       }
     }
 
     private void selectOrderList(Login user) {
@@ -44,7 +52,11 @@ public class OrderHistoryView {
             if ("0".equals(inputOrderId)) {
                 return;
             }
-            orderController.selectOneOrderDetailByOrderId(inputOrderId, simpleOrderList);
+            try {
+                orderController.selectOneOrderDetailByOrderId(inputOrderId, simpleOrderList);
+            } catch (IllegalArgumentException e) {
+                ExceptionPrinter.print(e.getMessage());
+            }
         }
     }
 
